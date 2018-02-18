b2c4bcf8c8df7d5c59977636e971c18ac009485d
Mika Lindell <mika.lindell82@gmail.com>
2018-02-18 16:03:02 +0200

    Configured Webpack to generate sandbox.html for the extension to run sandboxed actions

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -34,32 +34,35 @@ module.exports = {
   // These are the "entry points" to our application.
   // This means they will be the "root" imports that are included in JS bundle.
   // The first two entry points enable "hot" CSS and auto-refreshes for JS.
-  entry: [
-    // Include an alternative client for WebpackDevServer. A client's job is to
-    // connect to WebpackDevServer by a socket and get notified about changes.
-    // When you save a file, the client will either apply hot updates (in case
-    // of CSS changes), or refresh the page (in case of JS changes). When you
-    // make a syntax error, this client will display a syntax error overlay.
-    // Note: instead of the default WebpackDevServer client, we use a custom one
-    // to bring better experience for Create React App users. You can replace
-    // the line below with these two lines if you prefer the stock client:
-    // require.resolve('webpack-dev-server/client') + '?/',
-    // require.resolve('webpack/hot/dev-server'),
-    // Chrome extionsions need to run under chrome-extension:// -protocol
-    // However the host of the devServer is hardcoded to be window.location
-    // We need it to be something else, hence the need for fork
-    // require.resolve('react-dev-utils/webpackHotDevClient'),
-    require.resolve('./webpackHotDevClient.fork'),
-    // We ship a few polyfills by default:
-    require.resolve('./polyfills'),
-    // Enable hot-loading
-    require.resolve('react-hot-loader/patch'),
-    // Finally, this is your app's code:
-    paths.appIndexJs,
-    // We include the app code last so that if there is a runtime error during
-    // initialization, it doesn't blow up the WebpackDevServer client, and
-    // changing JS code would still trigger a refresh.
-  ],
+  entry: {
+    app: [
+      // Include an alternative client for WebpackDevServer. A client's job is to
+      // connect to WebpackDevServer by a socket and get notified about changes.
+      // When you save a file, the client will either apply hot updates (in case
+      // of CSS changes), or refresh the page (in case of JS changes). When you
+      // make a syntax error, this client will display a syntax error overlay.
+      // Note: instead of the default WebpackDevServer client, we use a custom one
+      // to bring better experience for Create React App users. You can replace
+      // the line below with these two lines if you prefer the stock client:
+      // require.resolve('webpack-dev-server/client') + '?/',
+      // require.resolve('webpack/hot/dev-server'),
+      // Chrome extensions need to run under chrome-extension:// -protocol
+      // However the host of the devServer is hardcoded to be window.location
+      // We need it to be something else, hence the need for fork
+      // require.resolve('react-dev-utils/webpackHotDevClient'),
+      require.resolve('./webpackHotDevClient.fork'),
+      // We ship a few polyfills by default:
+      require.resolve('./polyfills'),
+      // Enable hot-loading
+      require.resolve('react-hot-loader/patch'),
+      // Finally, this is your app's code:
+      paths.appIndexJs,
+      // We include the app code last so that if there is a runtime error during
+      // initialization, it doesn't blow up the WebpackDevServer client, and
+      // changing JS code would still trigger a refresh.
+    ],
+    sandbox: paths.sandboxIndexJs
+  },
   output: {
     // The dir devServer will make builds during development
     // Load this dir into Chrome as dev-extension
@@ -69,9 +72,9 @@ module.exports = {
     // This does not produce a real file. It's just the virtual path that is
     // served by WebpackDevServer in development. This is the JS bundle
     // containing code from all our entry points, and the Webpack runtime.
-    filename: 'static/js/bundle.js',
+    filename: 'static/js/[name].bundle.js',
     // There are also additional JS chunk files if you use code splitting.
-    chunkFilename: 'static/js/[name].chunk.js',
+    chunkFilename: 'static/js/[id].chunk.js',
     // This is the URL that app is served from. We use "/" in development.
     publicPath: publicPath,
     // Point sourcemap entries to original disk location (format as URL on Windows)
@@ -232,7 +235,16 @@ module.exports = {
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
       inject: true,
+      filename: 'index.html',
       template: paths.appHtml,
+      chunks: ['app'],
+    }),
+    // Generates an `sandbox.html` file with the sandbox-<script> injected.
+    new HtmlWebpackPlugin({
+      inject: true,
+      filename: 'sandbox.html',
+      template: paths.sandboxHtml,
+      chunks: ['sandbox'],
     }),
     // Add module names to factory functions so they appear in browser profiler.
     new webpack.NamedModulesPlugin(),
